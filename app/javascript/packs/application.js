c0057329e1b57c7a798d36d319e25489d1e4c891
Pauline Dussart <paulinedussart04@gmail.com>
2020-02-10 15:45:52 +0100

    The navbar is responsive and work very well, there is hover animation and not reload when change, there is one stuff to do: the active option

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
-require("@rails/ujs").start()
+// uncomment because big error already loaded
+// require("@rails/ujs").start()
+// const Rails = require('@rails/ujs');
+// Rails.start();
+
 require("@rails/activestorage").start()
 require("channels")
 
+
+
 import "bootstrap";
+import "../components/navbar";
