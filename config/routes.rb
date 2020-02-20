df3f692a7726272f511b385978b1493a9ed1e8bc
Pauline Dussart <paulinedussart04@gmail.com>
2020-02-20 22:19:04 +0100

    setup routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'portfolios/coworkspace'
-  get 'portfolios/marcelle'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   get 'portfolio', to: 'pages#portfolio', as: :portfolio
-  get 'coworkspace', to: 'pages#coworkspace', as: :coworkspace
-  get 'marcelle', to: 'pages#marcelle', as: :marcelle
+  get 'portfolio/coworkspace', to: 'portfolios#coworkspace'
+  get 'portfolio/marcelle', to: 'portfolios#marcelle'
+
 end
