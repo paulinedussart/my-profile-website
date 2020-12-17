b3e53fbddaa87033a908faff89ebfb253c0cc6af
Pauline Dussart <paulinedussart04@gmail.com>
2020-12-17 14:35:27 +0100

    add meta data for social network

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   #include HttpAuthConcern
-    http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD'], only: [:new, :create, :edit, :update]
-  end
+  http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD'], only: [:new, :create, :edit, :update]
+	
+	
+	def default_url_options
+	  { host: ENV["DOMAIN"] || "localhost:3000" }
+	end
+end
